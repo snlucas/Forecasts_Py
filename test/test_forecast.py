8196296e23aec6115a7ee3dc72622bcefad19625
lucas <lucas.lambda.101@gmail.com>
2021-04-09 20:58:28 -0300

    Start file for tests

---
+++ test/test_forecast.py
@@ -0,0 +1,7 @@
+import unittest
+from forecast import Forecast
+
+
+class TestForecast:
+    def test_forecasts_returns_dict(self):
+        pass
---
+++ test/test_forecast_controller.py
@@ -0,0 +1,34 @@
+import unittest
+from forecast_controller import ForecastController
+
+
+class TestForecastController:
+    def test_token_as_string_given_empty_string(self):
+        pass
+
+    def test_token_as_string_given_empty_input(self):
+        pass
+
+    def test_token_as_string_given_nonempty_input(self):
+        pass
+
+    def test_token_as_string_given_wrong_type_input(self):
+        pass
+
+    def test_token_as_file_given_empty_input(self):
+        pass
+
+    def test_token_as_file_given_nonempty_input(self):
+        pass
+
+    def test_token_as_file_given_wrong_type_input(self):
+        pass
+
+    def test_token_as_file_given_wrong_file_type(self):
+        pass
+
+    def test_city_forecast_given_wrong_type(self):
+        pass
+
+    def test_city_forecast_given_wrong_city_name(self):
+        pass
