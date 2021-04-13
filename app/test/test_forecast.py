d44273c63aafcb55e145ae495233bfb664302964
lucas <lucas.lambda.101@gmail.com>
2021-04-12 23:36:17 -0300

    Update test

--- app/test/test_forecast.py
+++ app/test/test_forecast.py
@@ -6,8 +6,8 @@ from controller.forecast import Forecast
 class TestForecast(TestCase):
     def test_execute_tests(self):
         self.test_forecasts_returns_dict()
-        self.test_day_forecast_set_string()
-        self.test_week_forecast_set_list()
+        self.test_day_forecast_return_string()
+        self.test_week_forecast_return_list()
 
     def test_forecasts_returns_dict(self):
         fc = ForecastController()
@@ -20,9 +20,8 @@ class TestForecast(TestCase):
         f = Forecast(token, city)
         self.assertEqual(type(f.forecasts()), dict, 'Wrong type')
 
-    def test_day_forecast_set_string(self):
-        # Test if the value changes,
-        # and if the value is a string
+    def test_day_forecast_return_string(self):
+        # Test if the value returns a string
 
         fc = ForecastController()
         fc_token = fc.token_as_file('token.txt')
@@ -32,15 +31,10 @@ class TestForecast(TestCase):
         city = fc.city
 
         f = Forecast(token, city)
-        day_forecast_init = f._day_forecast
-        fd = f.day_forecast()
-        day_forecast_later = f._day_forecast
-        self.assertNotEqual(day_forecast_init, day_forecast_later, 'Value hasnt changed')
-        self.assertIsInstance(day_forecast_later, str, 'Wrong type')
+        self.assertIsInstance(f.day_forecast(), str, 'Wrong type')
 
-    def test_week_forecast_set_list(self):
-        # Test if the value changes,
-        # and if the value is a list
+    def test_week_forecast_return_list(self):
+        # Test if the value returns a list
 
         fc = ForecastController()
         fc_token = fc.token_as_file('token.txt')
@@ -50,8 +44,4 @@ class TestForecast(TestCase):
         city = fc.city
 
         f = Forecast(token, city)
-        week_forecast_init = f._week_forecast
-        fw = f.week_forecast()
-        week_forecast_later = f._week_forecast
-        self.assertNotEqual(week_forecast_init, week_forecast_later, 'Value hasnt changed')
-        self.assertIsInstance(week_forecast_later, list, 'Wrong type')
+        self.assertIsInstance(f.week_forecast(), list, 'Wrong type')
